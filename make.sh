139ff96faa198e50fd2caaade39c7b75bdcdb794
Viacheslav Wolf <vwo@netlands.de>
2013-05-30 22:44:17 +0200

    Builder angepasst

--- make.sh
+++ make.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-#zip -r test.xpi *
+zip -r ffvcontactecontrol_$1.xpi * -x .git \*.xpi \*.bat \*.sh \*~
+
+exit;
 
 rsync -avrz . ~/.mozilla/firefox/bhyickmc.default/extensions/ffvcontactecontrol@killbar.org
 wmctrl -c Firefox
